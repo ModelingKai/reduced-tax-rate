7db26525c663ce2edac8e0b80fc0c19dafbf68c6
Jun Nakajima <ikuta1919@gmail.com>
2020-08-06 22:26:03 +0900

    酒類と医薬部外品のテストを追加

--- tests/test_こうなったらええやん.py
+++ tests/test_こうなったらええやん.py
@@ -7,7 +7,13 @@ from dataclasses import dataclass
 class 判定屋さん:
 
     def これって軽減税率ですか(self, item: 商品) -> bool:
-        return True
+        if item.hinmoku.name == '酒類':
+            return False
+        if item.hinmoku.name == '食料品':
+            return True
+
+        # あてはまらなかったら、軽減税率対象外
+        return False
 
 
 @dataclass
@@ -27,4 +33,18 @@ class Testこうなったらええやん:
 
         item_からあげ棒 = 商品(name='からあげ棒', hinmoku=品目('食料品'))
 
-        assert sut.これって軽減税率ですか(item_からあげ棒)
+        assert sut.これって軽減税率ですか(item_からあげ棒) is True
+
+    def test_ストロングゼロを指定して軽減税率の対象でないことがわかること(self):
+        sut = 判定屋さん()
+
+        item_ストロングゼロ = 商品(name='ストロングゼロ', hinmoku=品目('酒類'))
+
+        assert sut.これって軽減税率ですか(item_ストロングゼロ) is False
+
+    def test_医薬部外品を指定して軽減税率の対象でないことがわかること(self):
+        sut = 判定屋さん()
+
+        item_リポビタンD = 商品(name='リポビタンD', hinmoku=品目('医薬部外品'))
+
+        assert sut.これって軽減税率ですか(item_リポビタンD) is False
